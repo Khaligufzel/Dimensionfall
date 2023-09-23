54b12f060fe6c0fc3d304acc0a29fbaf5aaf266e
Jarosław Bandosz <jaroslaw.bandosz@gmail.com>
2023-09-23 13:08:14 +0200

    Loading crafting recipes from the JSON file

--- Scripts/crafting_recipes_manager.gd
+++ Scripts/crafting_recipes_manager.gd
@@ -1,9 +1,11 @@
 extends Node
 
+@onready var crafting_recipes
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_crafting_recipes_from_json()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,5 +16,4 @@ func get_crafting_recipes_from_json():
 	var file = "user://crafting_recipes.json"
 	var json_as_text = FileAccess.get_file_as_string(file)
 	var json_as_dict = JSON.parse_string(json_as_text)
-	if json_as_dict:
-		print(json_as_dict)
+	crafting_recipes = json_as_dict
