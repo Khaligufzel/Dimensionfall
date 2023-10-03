c09d265650649595f00a4f68e143211d84a15ba2
Jarosław Bandosz <jaroslaw.bandosz@gmail.com>
2023-10-03 02:17:44 +0200

    Better lighting. Z-level starting map

---
+++ LevelManager.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	for level in get_children():
+		if level.global_position.y > get_tree().get_first_node_in_group("Players").global_position.y:
+			level.visible = false
+		else:
+			level.visible = true
