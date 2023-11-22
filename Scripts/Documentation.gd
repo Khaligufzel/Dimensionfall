7f9de8f45087a3971b598ff94f8024cfa078f5aa
snipercup <50166150+snipercup@users.noreply.github.com>
2023-11-22 01:29:49 +0100

    Update doc menu layout

--- Scripts/Documentation.gd
+++ Scripts/Documentation.gd
@@ -1,10 +1,13 @@
 extends Control
 
+@export var categoryTree: Tree = null
+@export var documentDisplay: MarkdownLabel = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CategoryTree.hide_root = true
+	categoryTree.hide_root = true
 	load_documentation_files()
-	$CategoryTree.item_selected.connect(_on_CategoryTree_item_selected)
+	categoryTree.item_selected.connect(_on_CategoryTree_item_selected)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,7 +17,7 @@ func load_documentation_files():
 	var resourceDir = "./Documentation"
 	var dir = DirAccess.open(resourceDir)
 	if dir:
-		var root = $CategoryTree.create_item()
+		var root = categoryTree.create_item()
 		populateTree(resourceDir, root)
 	else:
 		print("An error occurred when trying to access the path.")
@@ -32,13 +35,13 @@ func populateTree(directory: String, treeItem: TreeItem):
 		while file_name != "":
 			var filePath = directory + "/" + file_name
 			if dir.current_is_dir():
-				var newTreeItem = $CategoryTree.create_item(treeItem)
+				var newTreeItem = categoryTree.create_item(treeItem)
 				newTreeItem.set_text(0, sanitize_filename(file_name))
 				populateTree(filePath, newTreeItem)
 			else:
 				var fileInfo = FileAccess.open(filePath, FileAccess.READ)
 				if fileInfo:
-					var newTreeItem = $CategoryTree.create_item(treeItem)
+					var newTreeItem = categoryTree.create_item(treeItem)
 					newTreeItem.set_text(0, sanitize_filename(file_name))
 					newTreeItem.set_metadata(0,filePath)
 				else:
@@ -53,11 +56,11 @@ func populateTree(directory: String, treeItem: TreeItem):
 #read the filename from the metadata of the selected Treeitem. 
 #It will then display the contents of the file
 func _on_CategoryTree_item_selected():
-	var selected_item = $CategoryTree.get_selected()
+	var selected_item = categoryTree.get_selected()
 	if selected_item != null:
 		var metadata = selected_item.get_metadata(0)
 		if metadata != null:
-			$DocumentDisplay.display_file(metadata)
+			documentDisplay.display_file(metadata)
 
 #This function takes a string and replaces "_" with " " and removes' ".md" at the end of the string. 
 #It then returns the modified string
