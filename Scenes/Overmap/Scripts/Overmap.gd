d5836a2f051778e0ca731a8ee6de50a4f7868f8e
snipercup <50166150+snipercup@users.noreply.github.com>
2023-11-29 20:46:26 +0100

    Infinite panning now possible

--- Scenes/Overmap/Scripts/Overmap.gd
+++ Scenes/Overmap/Scripts/Overmap.gd
@@ -10,11 +10,14 @@ var noise = FastNoiseLite.new()
 var chunk_height: int = 32
 var chunk_width: int = 32
 var chunk_size = 32
+var tile_size = 32
+var grid_pixel_size = chunk_size*tile_size
 var loaded_tiles: Array = []
 var tile_materials = {} # Create an empty dictionary to store materials
 
 #We will connect the position_coord to this function in the _ready function
 func _ready():
+	position_coord = positionLabel.position
 	load_tiles_material()
 	noise.seed = randi()
 	noise.fractal_octaves = 4
@@ -22,7 +25,7 @@ func _ready():
 	noise.fractal_gain = 0.5
 	update_chunks()
 	connect("position_coord_changed", on_position_coord_changed)
-	draw_all_chunks()
+#	draw_all_chunks()
 
 # The GDScript function `update_chunks()` updates the
 # chunks in a 2D game world. It loops through a 4x4 grid
@@ -34,43 +37,69 @@ func _ready():
 # and `position_coord` is the current position in the
 # game world.
 func update_chunks():
-	for x in range(-2, 2):
-		for y in range(-2, 2):
-			var chunk_position = position_coord + Vector2(x, y) * chunk_size
-			if not chunks.has(chunk_position):
-				generate_chunk(chunk_position, chunk_size)
-	unload_chunks()
+	# Convert the current camera or player position to grid coordinates based on the grid's pixel size (chunk size).
+	var grid_position = (position_coord / grid_pixel_size).floor() * grid_pixel_size
+	grid_position.x += grid_pixel_size+grid_pixel_size
+	grid_position.y += grid_pixel_size+grid_pixel_size
 
-# This GDScript function, `generate_chunk`, generates
-# a chunk of tiles at a given position with a specified
-# size. First, it initializes an empty list, `chunk`,
-# to store the tiles. Then, it iterates over the range
-# of the new size in both the x and y dimensions. For
-# each tile, it determines the tile type based on a 2D
-# noise function, which takes the x and y coordinates
-# (adjusted by the position of the chunk) as input. This
-# noise value is then normalized (to be within the range
-# of 0 to 1) and scaled by the size of the `tiles` array,
-# then converted to an integer to serve as an index for
-# the `tiles` array. The corresponding tile is added
-# to the `chunk` list. Finally, the function stores the
-# generated chunk in the `chunks` dictionary, using the
-# position of the chunk as the key. This function is
-# typically used in procedural generation of game maps,
-# where each "chunk" represents a portion of the game
-# world.
-func generate_chunk(position_loc: Vector2, newSize: int):
+	for x in range(-2, 2):  # Adjust the range based on how large you want the active area to be.
+		for y in range(-2, 2):
+			var chunk_grid_position = grid_position + Vector2(x, y) * grid_pixel_size
+			# Key used to store and look up the GridContainers within the chunks dictionary.
+			var key = chunk_grid_position
+#			var key = str(chunk_grid_position.x) + "," + str(chunk_grid_position.y)
+
+			if not chunks.has(key):
+				generate_chunk(chunk_grid_position)
+				# Within context of where you handle chunk generation or updating:
+				var chunk_data = chunks[chunk_grid_position]  # Retrieve the chunk data for the specific position.
+				# Use chunk data to create and fill the GridContainer.
+				var new_grid_container = create_and_fill_grid_container(chunk_data, Vector2(chunk_grid_position.x-position_coord.x,chunk_grid_position.y-position_coord.y))
+				tilesContainer.add_child(new_grid_container)
+				# Store the GridContainer using the grid position as the key.
+				chunks[key] = new_grid_container
+
+	# After generating new chunks, you may want to unload any that are off-screen.
+	unload_chunks()
+#
+#func update_chunks():
+#	var grid_position_coord = position_coord.snapped(Vector2(grid_pixel_size, grid_pixel_size))
+#	var current_chunk_pos = grid_position_coord
+#
+#	for x in range(-2, 2):
+#		for y in range(-2, 2):
+#			var chunk_position = current_chunk_pos# + Vector2(x*grid_pixel_size, y*grid_pixel_size)
+#			var world_chunk_position = chunk_position
+##			var world_chunk_position = chunk_position * chunk_size * tile_size
+##			var world_chunk_position = Vector2(grid_pixel_size*x, grid_pixel_size*y)
+#			var chunk_has_position: bool = chunks.has(world_chunk_position)
+#			if not chunk_has_position:
+#				generate_chunk(world_chunk_position)
+#				# Within context of where you handle chunk generation or updating:
+#				var chunk_data = chunks[chunk_position]  # Retrieve the chunk data for the specific position.
+#				var grid_container = create_and_fill_grid_container(chunk_data, chunk_position)
+#				tilesContainer.add_child(grid_container)  # Assuming tilesContainer is already added to the scene tree.
+##				draw_chunk(world_chunk_position, chunks[world_chunk_position])
+#	unload_chunks()
+
+#
+func generate_chunk(position_loc: Vector2):
 	var chunk = []
-	for x in range(newSize):
-		for y in range(newSize):
+	for x in range(chunk_size):
+		for y in range(chunk_size):
 			var tile_type = noise.get_noise_2d(position_loc.x + x, position_loc.y + y)
 			tile_type = int((tile_type + 1) / 2 * tiles.size())
 			chunk.append(tiles[tile_type])
 	chunks[position_loc] = chunk
 
 func unload_chunks():
+	var dist = 0
+	var range = 0
 	for chunk_position in chunks.keys():
-		if chunk_position.distance_to(position_coord) > 2 * chunk_size:
+		dist = chunk_position.distance_to(position_coord)
+		range = 8 * grid_pixel_size
+		if dist > range:
+			chunks[chunk_position].queue_free()
 			chunks.erase(chunk_position)
 
 var mouse_button_pressed: bool = false
@@ -78,80 +107,63 @@ var mouse_button_pressed: bool = false
 #We will emit this signal when the position_coords change
 signal position_coord_changed(delta)
 
-#We will change the position_coords in the _input function
+
 func _input(event):
 	if event is InputEventMouseButton:
 		match event.button_index:
 			MOUSE_BUTTON_MIDDLE: 
-				if event.pressed:
-					mouse_button_pressed = true
-				else:
-					mouse_button_pressed = false
-	
-	if event is InputEventMouseMotion:
-		if mouse_button_pressed:
-			# Calculate the new position first.
-			var new_position_coord = position_coord - event.relative / 100
-			# Now calculate delta based on the old and new positions.
-			var delta = new_position_coord - last_position_coord
-			# Update position_coord to the new position
+				mouse_button_pressed = event.is_pressed()
+
+	if event is InputEventMouseMotion and mouse_button_pressed:
+		# Adjust the position based on the mouse movement, divided by 100 for sensitivity.
+		var motion = event.relative / 2
+
+		# Calculate the new position first.
+		var new_position_coord = position_coord - motion
+
+		# Round the new_position_coord to the nearest integer.
+		new_position_coord = new_position_coord.round()
+
+		# Calculate the delta based on the old and the rounded new positions.
+		var delta = new_position_coord - position_coord
+
+		if delta != Vector2.ZERO:
+			# Update position_coord to the new rounded position.
 			position_coord = new_position_coord
-			# Pass the delta when emitting the signal
+
+			# Emit the signal to update other parts of the game that depend on the position.
 			emit_signal("position_coord_changed", delta)
+
+			# Call update_tiles_position to move the tiles on the screen.
+#			update_tiles_position(delta)
+
 			# Update last_position_coord for the next input event.
 			last_position_coord = position_coord
-			update_chunks()
-#			var delta = position_coord - last_position_coord
-#			print_debug(delta)
-#			position_coord += event.relative / 100
-#			emit_signal("position_coord_changed", delta)
-#			last_position_coord = position_coord
-#			update_chunks()
-#			position_coord.x += event.relative.x / 100
-#			position_coord.y += event.relative.y / 100
-#			emit_signal("position_coord_changed")
-#			update_chunks()
 
-#This function is never called but is used to draw tile onto the screen
-#We need to find when to call this function, probably after chunks are updated
-func draw_all_chunks():
-	for chunk_position in chunks.keys():
-		var chunk = chunks[chunk_position]
-		for i in range(chunk.size()):
-			var tile_position = chunk_position + Vector2(i % 32, i / 32)
-			var tile_type = chunk[i]
-			var texture = tile_materials[tile_type]
-#			var texture = load("res://Mods/Core/OvermapTiles/" + tile_type + ".png")
-			var tile = TextureRect.new()
-			tile.texture = texture
-			tile.position = tile_position * 32
-			tilesContainer.add_child(tile)
-			loaded_tiles.append(tile)
-	if positionLabel:
-		positionLabel.text = "Position: " + str(position_coord)
+			# Update the chunks based on the new position.
+			update_chunks()
 
 
 #This function will move all the tiles on screen when the position_coords change
 #This will make it look like the user pans across the map
 #When tiles move too far away the should be unloaded
-
 func update_tiles_position(delta):
-	for tile in loaded_tiles:
-		# Update each tile position by subtracting the delta to move them relative to the camera's movement.
-		tile.position -= delta * chunk_size
-		# Check if the tile has gone off the screen and should be unloaded.
-		if tile.position.x < -chunk_size or \
-		tile.position.x > get_viewport().size.x + chunk_size or \
-		tile.position.y < -chunk_size or \
-		tile.position.y > get_viewport().size.y + chunk_size:
-			tile.queue_free()
-			loaded_tiles.erase(tile)
-#func update_tiles_position():
-#	for tile in loaded_tiles:
-#		tile.position += position_coord# * chunk_size
-#		if tile.position.x < -chunk_size or tile.position.x > get_viewport().size.x + chunk_size or tile.position.y < -chunk_size or tile.position.y > get_viewport().size.y + chunk_size:
-#			tile.queue_free()
-#			loaded_tiles.erase(tile)
+	var dist = Vector2(3 * grid_pixel_size + position_coord.x, 3 * grid_pixel_size + position_coord.x)
+	for grid_container in tilesContainer.get_children():
+		# Check if the node is indeed a GridContainer since the container might have other types of nodes.
+		if grid_container is GridContainer:
+			# Update the grid container's position by subtracting the delta to move it relative to the camera's movement.
+			grid_container.position -= delta
+
+			# Remove GridContainer if it is too far from the viewport (off-screen)
+			# You need to define the bounds based on your game's design
+#			var grid_container_global_position = tilesContainer.rect_global_position + grid_container.rect_position
+#			var grid_container_global_position = grid_container.position
+#
+#			# Change these values according to your viewport and map setup
+#			if grid_container_global_position.x < -dist.x or grid_container_global_position.x > get_viewport().size.x + dist.x or grid_container_global_position.y < -dist.y or grid_container_global_position.y > get_viewport().size.y + dist.y:
+#				grid_container.queue_free()  # Use queue_free() to safely delete the node
+
 
 #We will call this function when the position_coords change
 func on_position_coord_changed(delta):
@@ -159,12 +171,31 @@ func on_position_coord_changed(delta):
 	update_chunks()
 	if positionLabel:
 		positionLabel.text = "Position: " + str(position_coord)
-#func on_position_coord_changed():
-#	update_tiles_position()
-#	update_chunks()
-#	if positionLabel:
-#		positionLabel.text = "Position: " + str(position_coord)
+		
+		
+func create_and_fill_grid_container(chunk: Array, chunk_position: Vector2):
+	# Create a new GridContainer node for this chunk.
+	var grid_container = GridContainer.new()
+	grid_container.columns = chunk_width  # Set the number of columns to chunk_width.
+	grid_container.set("theme_override_constants/h_separation", 0)
+	grid_container.set("theme_override_constants/v_separation", 0)
 
+	# Calculate the starting position for this chunk's set of tiles.
+	var chunk_pixel_position = chunk_position# * chunk_size * tile_size
+
+	# Iterate over the chunk array to create and add TextureRects for each tile.
+	for i in range(chunk.size()):
+		var tile_type = chunk[i]
+		var texture = tile_materials[tile_type] # Retrieve the texture based on the tile type.
+		var tile = TextureRect.new()
+		tile.texture = texture
+		grid_container.add_child(tile)
+
+	# Set the position of the grid container in pixel space.
+	grid_container.position = chunk_pixel_position
+
+	# Return the filled grid container.
+	return grid_container
 
 
 # This function reads all the files in "res://Mods/Core/Tiles/". It will check if the file is a .png file. If the file is a .png file, it will create a new material with that .png image as the texture. It will put all of the created materials in a dictionary with the name of the file as the key and the material as the value.
@@ -185,4 +216,5 @@ func load_tiles_material():
 	else:
 		print_debug("An error occurred when trying to access the path.")
 	dir.list_dir_end()
+
 
