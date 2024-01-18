ff7eea040e9f6cc57207b28db32992c3a40b3e8d
snipercup <50166150+snipercup@users.noreply.github.com>
2024-01-18 16:10:57 +0100

    Furniture above player invisible, fix bug

--- LevelManager.gd
+++ LevelManager.gd
@@ -1,15 +1,26 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
+  # Initialize with a value that's unlikely to be a valid Y-level
+var last_player_y_level: float = -1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	var player = get_tree().get_first_node_in_group("Players")
+	if player:
+		var current_player_y = player.global_position.y
+
+		# Check if the player's Y-level has changed
+		if current_player_y != last_player_y_level:
+			update_visibility(current_player_y)
+			last_player_y_level = current_player_y
+
+func update_visibility(player_y: float):
+	# Update level visibility
 	for level in get_children():
-		if level.global_position.y > get_tree().get_first_node_in_group("Players").global_position.y:
-			level.visible = false
-		else:
-			level.visible = true
+		var is_above_player = level.global_position.y > player_y
+		level.visible = not is_above_player
+
+	# Update furniture visibility
+	for furniture in get_tree().get_nodes_in_group("furniture"):
+		var is_above_player = furniture.global_position.y > player_y
+		furniture.visible = not is_above_player
