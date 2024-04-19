7de61fe3ac6e48fc4c2200164dc476e9b75b03a0
snipercup <50166150+snipercup@users.noreply.github.com>
2024-04-19 15:48:46 +0200

    Fix furniture spawning, player interaction with furniture

--- Scripts/FurniturePhysics.gd
+++ Scripts/FurniturePhysics.gd
@@ -17,7 +17,9 @@ var current_health: float = 10.0
 
 
 func _ready():
-	start_timer()
+	set_position.call_deferred(furnitureposition)
+	set_new_rotation(furniturerotation)
+	last_rotation = furniturerotation
 
 
 # Keep track of the furniture's position and rotation. It starts at 0,0,0 and the moves to it's
@@ -28,23 +30,6 @@ func _physics_process(_delta):
 		_moved(furnitureposition, global_transform.origin)
 	if rotation_degrees.y != last_rotation:
 		last_rotation = rotation_degrees.y
-
-
-# Function to create and start a timer that will generate chunks every 1 second if applicable
-func start_timer():
-	var my_timer = Timer.new() # Create a new Timer instance
-	my_timer.wait_time = 1 # Timer will tick every 1 second
-	my_timer.one_shot = true # False means the timer will repeat
-	add_child(my_timer) # Add the Timer to the scene as a child of this node
-	my_timer.timeout.connect(_on_Timer_timeout) # Connect the timeout signal
-	my_timer.start() # Start the timer
-
-
-# This function will be called every time the Timer ticks
-func _on_Timer_timeout():
-	set_position.call_deferred(furnitureposition)
-	set_new_rotation(furniturerotation)
-	last_rotation = furniturerotation
 
 
 func get_hit(damage):
