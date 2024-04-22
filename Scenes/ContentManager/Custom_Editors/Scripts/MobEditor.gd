fd30deefccdd93c9a2a8d4abd6f8a92fc4407f36
snipercup <50166150+snipercup@users.noreply.github.com>
2024-04-22 20:27:41 +0200

    Mobeditor can have loot group assigned to mob

--- Scenes/ContentManager/Custom_Editors/Scripts/MobEditor.gd
+++ Scenes/ContentManager/Custom_Editors/Scripts/MobEditor.gd
@@ -19,6 +19,7 @@ extends Control
 @export var sightRange_numedit: SpinBox
 @export var senseRange_numedit: SpinBox
 @export var hearingRange_numedit: SpinBox
+@export var ItemGroupTextEdit: TextEdit = null
 # This signal will be emitted when the user presses the save button
 # This signal should alert Gamedata that the mob data array should be saved to disk
 # The content editor has connected this signal to Gamedata already
@@ -60,6 +61,8 @@ func load_mob_data() -> void:
 		senseRange_numedit.get_line_edit().text = contentData["sense_range"]
 	if hearingRange_numedit != null and contentData.has("hearing_range"):
 		hearingRange_numedit.get_line_edit().text = contentData["hearing_range"]
+	if ItemGroupTextEdit != null and contentData.has("loot_group"):
+		ItemGroupTextEdit.text = contentData["loot_group"]
 	
 
 #The editor is closed, destroy the instance
@@ -83,6 +86,10 @@ func _on_save_button_button_up() -> void:
 	contentData["sight_range"] = sightRange_numedit.get_line_edit().text
 	contentData["sense_range"] = senseRange_numedit.get_line_edit().text
 	contentData["hearing_range"] = hearingRange_numedit.get_line_edit().text
+	if ItemGroupTextEdit.text:
+		contentData["loot_group"] = ItemGroupTextEdit.text
+	else:
+		contentData.erase("loot_group")
 	data_changed.emit()
 
 #When the mobImageDisplay is clicked, the user will be prompted to select an image from 
@@ -96,3 +103,45 @@ func _on_sprite_selector_sprite_selected_ok(clicked_sprite) -> void:
 	var mobTexture: Resource = clicked_sprite.get_texture()
 	mobImageDisplay.texture = mobTexture
 	PathTextLabel.text = mobTexture.resource_path.get_file()
+
+
+
+
+# This function should return true if the dragged data can be dropped here
+func _can_drop_data(_newpos, data) -> bool:
+	# Check if the data dictionary has the 'id' property
+	if not data or not data.has("id"):
+		return false
+	
+	# Fetch itemgroup data by ID from the Gamedata to ensure it exists and is valid
+	var item_data = Gamedata.get_data_by_id(Gamedata.data.itemgroups, data["id"])
+	if item_data.is_empty():
+		return false
+
+	# If all checks pass, return true
+	return true
+
+
+# This function handles the data being dropped
+func _drop_data(newpos, data) -> void:
+	if _can_drop_data(newpos, data):
+		_handle_item_drop(data, newpos)
+
+
+# Called when the user has successfully dropped data onto the ItemGroupTextEdit
+# We have to check the dropped_data for the id property
+func _handle_item_drop(dropped_data, _newpos) -> void:
+	# Assuming dropped_data is a Dictionary that includes an 'id'
+	if dropped_data and "id" in dropped_data:
+		var item_id = dropped_data["id"]
+		var item_data = Gamedata.get_data_by_id(Gamedata.data.itemgroups, item_id)
+		if item_data.is_empty():
+			print_debug("No item data found for ID: " + item_id)
+			return
+		ItemGroupTextEdit.text = item_id
+	else:
+		print_debug("Dropped data does not contain an 'id' key.")
+
+
+func _on_item_group_clear_button_button_up():
+	ItemGroupTextEdit.clear()
