0f0ed86ab90fce4f183358e70569d3ebbf3ef087
snipercup <50166150+snipercup@users.noreply.github.com>
2024-05-24 18:40:39 +0200

    Can scroll the inventory list, prevent zoom

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,8 +1,18 @@
 extends Camera3D
 
+var can_zoom: bool = true
+
+func _ready():
+	# We connect to the inventory visibility change to interrupt zooming
+	Helper.signal_broker.inventory_window_visibility_changed.connect(_on_inventory_visibility_change)
+
 func _input(event):
-	if event.is_action_pressed("zoom_in"):
+	if event.is_action_pressed("zoom_in") and can_zoom:
 		size -= 2
 		
-	if event.is_action_pressed("zoom_out"):
+	if event.is_action_pressed("zoom_out") and can_zoom:
 		size += 2
+
+# When the inventory is opened, stop zooming
+func _on_inventory_visibility_change(inventoryWindow):
+	can_zoom = not inventoryWindow.visible
