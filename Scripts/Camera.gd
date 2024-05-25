44074632c576704269804467c064fd65af54cccd
Jarosław Bandosz <jaroslaw.bandosz@gmail.com>
2024-05-25 02:46:59 +0200

    Camera is no longer orthogonal. Added max and min zoom.

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -8,10 +8,12 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("zoom_in") and can_zoom:
-		size -= 2
+		if position.y >= 2:
+			position.y -= 1
 		
 	if event.is_action_pressed("zoom_out") and can_zoom:
-		size += 2
+		if position.y <= 20:
+			position.y += 1	
 
 # When the inventory is opened, stop zooming
 func _on_inventory_visibility_change(inventoryWindow):
