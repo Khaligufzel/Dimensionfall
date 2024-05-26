fd70d8153d5db18ea227598dc061fe724442d157
Jarosław Bandosz <jaroslaw.bandosz@gmail.com>
2024-05-26 19:38:24 +0200

    Added zoom to world camera

--- WorldGeneratorTesting/SimpleCameraMove.gd
+++ WorldGeneratorTesting/SimpleCameraMove.gd
@@ -5,14 +5,16 @@ extends CharacterBody2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    var input_dir = Input.get_vector("left", "right", "up", "down")
-    velocity = input_dir.normalized() * speed
-    move_and_slide()
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	velocity = input_dir.normalized() * speed
+	move_and_slide()
 
-# Optional: If you want to handle physics in _physics_process instead
-# Uncomment the following lines and comment out the _process function above
 
-# func _physics_process(delta):
-#     var input_dir = Input.get_vector("left", "right", "up", "down")
-#     var velocity = input_dir.normalized() * speed
-#     velocity = move_and_slide(velocity)
+func _input(event):
+	if event.is_action_pressed("zoom_in") :
+		get_viewport().get_camera_2d().zoom.x += 0.1
+		get_viewport().get_camera_2d().zoom.y += 0.1
+			
+	if event.is_action_pressed("zoom_out"):
+		get_viewport().get_camera_2d().zoom.x -= 0.1
+		get_viewport().get_camera_2d().zoom.y -= 0.1
