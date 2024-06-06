4e7b3c22bc35e42274d2a945d133136ce7c6e678
snipercup <50166150+snipercup@users.noreply.github.com>
2024-06-07 01:19:54 +0200

    Refactor player skills, add xp

--- Scripts/CharacterWindow.gd
+++ Scripts/CharacterWindow.gd
@@ -14,13 +14,22 @@ func _ready():
 	_on_player_skill_changed(playerInstance)
 
 
+
+
+# Utility function to clear all children in a container
+func clear_container(container: Control):
+	for child in container.get_children():
+		child.queue_free()
+
+
 # Handles the update of the stats display when player stats change
 func _on_player_stat_changed(player_node: CharacterBody3D):
 	clear_container(statsContainer)  # Clear existing content
-	for stat_id in player_node.stats:
+	var playerstats = player_node.stats
+	for stat_id in playerstats:
 		var stat_data = Gamedata.get_data_by_id(Gamedata.data.stats, stat_id)
 		if stat_data:
-			var stat_entry = create_stat_or_skill_entry(stat_data, player_node.stats[stat_id], "stats")
+			var stat_entry = create_stat_or_skill_entry(stat_data, playerstats[stat_id], "stats")
 			statsContainer.add_child(stat_entry)
 
 
@@ -30,26 +39,25 @@ func _on_player_skill_changed(player_node: CharacterBody3D):
 	for skill_id in player_node.skills:
 		var skill_data = Gamedata.get_data_by_id(Gamedata.data.skills, skill_id)
 		if skill_data:
-			var skill_entry = create_stat_or_skill_entry(skill_data, player_node.skills[skill_id], "skills")
+			var skill_value = player_node.skills[skill_id]
+			var skill_entry = create_stat_or_skill_entry(skill_data, skill_value, "skills")
 			skillsContainer.add_child(skill_entry)
 
-
 # Utility function to create an HBoxContainer for a stat or skill entry
-func create_stat_or_skill_entry(data: Dictionary, value: int, type: String) -> HBoxContainer:
+func create_stat_or_skill_entry(data: Dictionary, value: Variant, type: String) -> HBoxContainer:
 	var hbox = HBoxContainer.new()
 	var icon = TextureRect.new()
 	icon.texture = Gamedata.get_sprite_by_id(Gamedata.data[type], data["id"])  # Fetch sprite using the ID
 	hbox.add_child(icon)
 
 	var label = Label.new()
-	label.text = data["name"] + ": " + str(value)
+	if type == "skills":
+		# For skills, display level and XP
+		label.text = data["name"] + ": Level " + str(value["level"]) + ", XP: " + str(value["xp"])
+	else:
+		# For stats, display the value directly
+		label.text = data["name"] + ": " + str(value)
 	label.tooltip_text = data["description"]
 	hbox.add_child(label)
 
 	return hbox
-
-
-# Utility function to clear all children in a container
-func clear_container(container: Control):
-	for child in container.get_children():
-		child.queue_free()
