de924816cee2d6d9c9d1eda16fc17b548519e2a4
snipercup <50166150+snipercup@users.noreply.github.com>
2024-06-28 02:10:01 +0200

    Connect signals

--- Scripts/Helper/quest_helper.gd
+++ Scripts/Helper/quest_helper.gd
@@ -10,6 +10,15 @@ func _ready():
 	
 	# Connect to the Helper.signal_broker.game_loaded signal
 	Helper.signal_broker.game_loaded.connect(_on_game_loaded)
+	
+	# Connect to the QuestManager signals
+	QuestManager.quest_complete.connect(_on_quest_complete)
+	QuestManager.quest_failed.connect(_on_quest_failed)
+	QuestManager.step_complete.connect(_on_step_complete)
+	QuestManager.next_step.connect(_on_next_step)
+	QuestManager.step_updated.connect(_on_step_updated)
+	QuestManager.new_quest_added.connect(_on_new_quest_added)
+	QuestManager.quest_reset.connect(_on_quest_reset)
 
 # Function for handling game started signal
 func _on_game_started():
@@ -21,27 +30,65 @@ func _on_game_loaded():
 	# To be developed later
 	pass
 
+# Function to handle quest completion
+func _on_quest_complete(_quest: Dictionary):
+	# To be developed later
+	pass
+
+# Function to handle quest failure
+func _on_quest_failed(_quest: Dictionary):
+	# To be developed later
+	pass
+
+# Function to handle step completion
+func _on_step_complete(_step: Dictionary):
+	# To be developed later
+	pass
+
+# Function to handle next step
+func _on_next_step(_step: Dictionary):
+	# To be developed later
+	pass
+
+# Function to handle step update
+func _on_step_updated(_step: Dictionary):
+	# To be developed later
+	pass
+
+# Function to handle new quest addition
+func _on_new_quest_added(_quest_name: String):
+	# To be developed later
+	pass
+
+
+# Function to handle quest reset
+func _on_quest_reset(_quest_name: String):
+	# To be developed later
+	pass
+
 
 func initialize_quests():
+	QuestManager.wipe_all_quest_data()
 	var quest_data = Gamedata.data.quests.data
 	for quest in quest_data:
 		create_quest_from_data(quest)
 
 
-func create_quest_from_data(quest_data):
+func create_quest_from_data(quest_data: Dictionary):
 	if quest_data.steps.len < 1:
 		return # The quest has no steps
-	var Quest = ScriptQuest.new(quest_data.name,quest_data.description)
+	var Quest = ScriptQuest.new(quest_data.id, quest_data.description)
 	var steps_added: bool = false
 	var steps = quest_data.steps
 	for step in steps:
 		if step.type == "collect":
-			#add an incremental step
+			# Add an incremental step
 			Quest.add_incremental_step("Gather items", step.item, step.amount)
 			steps_added = true
-	
+
 	if steps_added:
-		#finalize
+		Quest.set_quest_meta_data(quest_data) # The json data that defines the quest
+		# Finalize
 		Quest.finalize_quest()
-		#Add quest to player quests
+		# Add quest to player quests
 		QuestManager.add_scripted_quest(Quest)
--- Scripts/QuestWindow.gd
+++ Scripts/QuestWindow.gd
@@ -1,7 +1,15 @@
 extends Control
 
+# This script is supposed to work with QuestWindow.tscn.
+# It helps the player to manage quests
+# It has controls to select, assign and abandon quests
+# It has controls to view the quest details
+# It has tabs for current, completed and failed quests
+
+
 # Main Quest Journal Window
 @export var quest_overview_tabs: TabContainer
+@export var available_quests_list: ItemList
 @export var current_quests_list: ItemList
 @export var completed_quests_list: ItemList
 @export var failed_quests_list: ItemList
@@ -12,15 +20,69 @@ func _ready():
 	current_quests_list.item_selected.connect(_on_quest_selected)
 	completed_quests_list.item_selected.connect(_on_quest_selected)
 	failed_quests_list.item_selected.connect(_on_quest_selected)
+	
+	# Connect to the QuestManager signals
+	QuestManager.quest_complete.connect(_on_quest_complete)
+	QuestManager.quest_failed.connect(_on_quest_failed)
+	QuestManager.step_complete.connect(_on_step_complete)
+	QuestManager.next_step.connect(_on_next_step)
+	QuestManager.step_updated.connect(_on_step_updated)
+	QuestManager.new_quest_added.connect(_on_new_quest_added)
+	QuestManager.quest_reset.connect(_on_quest_reset)
 
-func _on_tab_changed(tab):
+
+# Function to handle quest completion
+func _on_quest_complete(_quest: Dictionary):
+	# To be developed later
+	pass
+
+
+# Function to handle quest failure
+func _on_quest_failed(_quest: Dictionary):
+	# To be developed later
+	pass
+
+
+# Function to handle step completion
+func _on_step_complete(_step: Dictionary):
+	# To be developed later
+	pass
+
+
+# Function to handle next step
+func _on_next_step(_step: Dictionary):
+	# To be developed later
+	pass
+
+
+# Function to handle step update
+func _on_step_updated(_step: Dictionary):
+	# To be developed later
+	pass
+
+
+# Function to handle new quest addition
+func _on_new_quest_added(_quest_name: String):
+	# To be developed later
+	pass
+
+
+# Function to handle quest reset
+func _on_quest_reset(_quest_name: String):
+	# To be developed later
+	pass
+
+
+func _on_tab_changed(_tab):
 	# Handle tab change logic
 	pass
+
 
 func _on_quest_selected(index):
 	# Update the quest details section based on selected quest
 	var selected_quest = current_quests_list.get_item_text(index) # Example for current quests
 	_update_quest_details(selected_quest)
+
 
 func _update_quest_details(quest_title):
 	quest_details_section.get_node("QuestTitle").text = quest_title
