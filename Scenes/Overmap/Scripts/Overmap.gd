2466454bf9ed36a790a5be98f97de0e8580943c4
snipercup <50166150+snipercup@users.noreply.github.com>
2024-07-15 16:17:37 +0200

    Remove Helper.chunks

--- Scenes/Overmap/Scripts/Overmap.gd
+++ Scenes/Overmap/Scripts/Overmap.gd
@@ -19,7 +19,7 @@ var previous_visible_tile: Control = null
 # We will emit this signal when the position_coords change
 # Which happens when the user has panned the overmap
 signal position_coord_changed(delta: Vector2)
-#Fires when the player has pressed the travel button
+# Fires when the player has pressed the travel button
 signal change_level_pressed()
 
 func _ready():
@@ -57,18 +57,12 @@ func update_chunks():
 			# At 0,0 we will have positions -64,-64 and -64, -32 and -64, 0 etc.
 			var chunk_grid_position: Vector2 = grid_position + Vector2(x, y) * chunk_size
 			print_debug("Creating chunk in overmap at " + str(chunk_grid_position))
-			
-			# Use the separate Dictionary for retrieving the noise data
-			if not Helper.chunks.has(chunk_grid_position):
-				generate_chunk(chunk_grid_position)
-			# Retrieve the chunk data for the specific position.
-			var chunk_data = Helper.chunks[chunk_grid_position]
 
 			if not grid_chunks.has(chunk_grid_position):
-				# Use chunk data to create and fill the GridContainer.
-				var localized_x: float = chunk_grid_position.x*tile_size - Helper.position_coord.x * tile_size
-				var localized_y: float = chunk_grid_position.y*tile_size - Helper.position_coord.y * tile_size
-				var new_grid_container = create_and_fill_grid_container(chunk_data, Vector2(localized_x, localized_y))
+				# Directly create and fill the GridContainer with chunk data.
+				var localized_x: float = chunk_grid_position.x * tile_size - Helper.position_coord.x * tile_size
+				var localized_y: float = chunk_grid_position.y * tile_size - Helper.position_coord.y * tile_size
+				var new_grid_container = create_and_fill_grid_container(chunk_grid_position, Vector2(localized_x, localized_y))
 				tilesContainer.call_deferred("add_child", new_grid_container)
 				# Store the GridContainer using the grid position as the key.
 				grid_chunks[chunk_grid_position] = new_grid_container
@@ -77,29 +71,9 @@ func update_chunks():
 	unload_chunks()
 
 
-# This function creates terrain for a specific area on the overmap. It uses a grid_position 
-# to determine where to generate the terrain. The function employs a noise algorithm 
-# to select tile types from a predefined list, creating a chunk of terrain data. 
-# This data is stored in a global dictionary for later use in rendering the overmap.
-func generate_chunk(grid_position: Vector2) -> void:
-	var chunk = []
-	for y in range(chunk_size):  # x goes from 0 to chunk_size - 1
-		for x in range(chunk_size):  # y goes from 0 to chunk_size - 1
-			# We calculate global coordinates by 
-			# offsetting the local coordinates by the grid_position
-			var global_x = grid_position.x + x # at 0,0 it will be between 0 and 15
-			var global_y = grid_position.y + y
-			if global_x == 0 and global_y == 0:
-				chunk.append({"global_x": global_x, "global_y": global_y})
-			else:
-				chunk.append({"global_x": global_x, "global_y": global_y})
-	# Store the chunk using the grid_position as the key.
-	Helper.chunks[grid_position] = chunk
-
-
 # The user will leave chunks behind as the map is panned around
 # Chunks that are too far from the current position will be destroyed
-# This will only destroy the visual representation of the data stored in Helper.chunks
+# This will only destroy the visual representation of the data.
 func unload_chunks():
 	# Lowering this number 5 will cause newly created chunks 
 	# to be instantly deleted and recreated
@@ -110,6 +84,7 @@ func unload_chunks():
 			grid_chunks[chunk_position].call_deferred("queue_free")
 			# Remove the reference to the grid
 			grid_chunks.erase(chunk_position)
+
 
 # Function to handle keyboard input for moving the overmap
 func _input(event):
@@ -157,52 +132,39 @@ func on_position_coord_changed(delta: Vector2):
 		positionLabel.text = "Position: " + str(Helper.position_coord)
 
 
-# This function creates and populates a GridContainer with tiles based on chunk data. 
-# It takes two arguments: chunk, an array containing data for each tile in the chunk, 
-# and chunk_position, a Vector2 representing the chunk's position in the world. 
-# The function generates a new GridContainer, sets its columns to chunk_width, and 
-# ensures no space between tiles. It then iterates over the chunk array, creating 
-# a tile for each entry. Each tile's metadata is set with global and local positions, 
-# and additional data like map files if available. Tiles are added as children to 
-# the GridContainer, which is positioned based on chunk_position. The function returns 
-# the populated GridContainer. This process visually represents a section of the 
-# overmap in a grid format.
-func create_and_fill_grid_container(chunk: Array, chunk_position: Vector2):
+# This function creates and populates a GridContainer with tiles based on chunk size and position.
+# The function generates a new GridContainer, sets its columns to chunk_width, and ensures no space between tiles.
+# It then generates terrain for each tile based on a noise algorithm and assigns metadata to each tile.
+# Tiles are added as children to the GridContainer, which is positioned based on chunk_position.
+# The function returns the populated GridContainer.
+func create_and_fill_grid_container(grid_position: Vector2, chunk_position: Vector2):
 	var grid_container = GridContainer.new()
 	grid_container.columns = chunk_width  # Set the number of columns to chunk_width.
 	# Make sure there is no space between the tiles
 	grid_container.set("theme_override_constants/h_separation", 0)
 	grid_container.set("theme_override_constants/v_separation", 0)
 
-	# Variables to keep track of the row and column position
-	var row: int = 0
-	var column: int = 0
+	# Iterate over the chunk size to create and add TextureRects for each tile.
+	for y in range(chunk_size):
+		for x in range(chunk_size):
 
-	# Iterate over the chunk array to create and add TextureRects for each tile.
-	for i in range(chunk.size()):
-		if i > 0 and i % chunk_width == 0:
-			row += 1
-			column = 0  # Reset column at the start of a new row
-		# Retrieve the texture based on the tile type.
-		var tile = overmapTile.instantiate()
-		var local_pos = Vector2(column * tile_size, row * tile_size)
-		var global_pos = Vector2(chunk[i].global_x, chunk[i].global_y)
-		var map_cell = Helper.overmap_manager.get_map_cell_by_local_coordinate(global_pos)
-		var texture: Texture = map_cell.get_sprite() if map_cell else null
-		tile.set_texture(texture)
-		# Assign the tile's row and column information
-		tile.set_meta("global_pos", global_pos)
-		tile.set_meta("local_pos", local_pos)
+			var tile = overmapTile.instantiate()
+			var local_pos = Vector2(x * tile_size, y * tile_size)
+			var global_pos = grid_position + Vector2(x, y)
 
-		if global_pos == Vector2.ZERO:
-			tile.set_color(Color(0.3, 0.3, 1))  # blue color
+			# Set the tile's metadata and texture based on global position
+			var map_cell = Helper.overmap_manager.get_map_cell_by_local_coordinate(global_pos)
+			var texture: Texture = map_cell.get_sprite() if map_cell else null
+			tile.set_texture(texture)
+			tile.set_meta("global_pos", global_pos)
+			tile.set_meta("local_pos", local_pos)
 
-		tile.tile_clicked.connect(_on_tile_clicked)
-		# Add the tile as a child to the grid container
-		grid_container.add_child(tile)
+			if global_pos == Vector2.ZERO:
+				tile.set_color(Color(0.3, 0.3, 1))  # blue color
 
-		# Increase column count after placing each tile
-		column += 1
+			tile.tile_clicked.connect(_on_tile_clicked)
+			# Add the tile as a child to the grid container
+			grid_container.add_child(tile)
 
 	# Set the position of the grid container in pixel space.
 	grid_container.position = chunk_position
@@ -282,6 +244,6 @@ func get_overmap_tile_at_position(myposition: Vector2) -> Control:
 
 
 # When the player moves a coordinate on the map, i.e. when crossing the chunk border.
-# Move ment could be between (0,0) and (0,1) for example
+# Movement could be between (0,0) and (0,1) for example
 func on_player_coord_changed(_player: CharacterBody3D, _old_pos: Vector2, new_pos: Vector2):
 	update_overmap_tile_visibility(new_pos)
