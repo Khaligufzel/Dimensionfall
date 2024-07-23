4e9713763fc7b776d204dce2901ed709e9cf5a24
snipercup <50166150+snipercup@users.noreply.github.com>
2024-07-23 11:54:59 +0200

    Update to the correct properties

--- Scripts/Gamedata/DItem.gd
+++ Scripts/Gamedata/DItem.gd
@@ -9,137 +9,205 @@ extends RefCounted
 var id: String
 var name: String
 var description: String
-var categories: Array
-var moveable: bool
 var weight: float
-var edgesnapping: String
+var volume: float
 var sprite: Texture
 var spriteid: String
-var function: Function
-var support_shape: SupportShape
-var destruction: Destruction
-var disassembly: Disassembly
+var image: String
+var stack_size: int
+var max_stack_size: int
+var two_handed: bool
 var references: Dictionary = {}
 
+# Other properties per type
+var craft: Craft
+var magazine: Magazine
+var ranged: Ranged
+var melee: Melee
+var food: Food
+var wearable: Wearable
 
-# Inner class to handle the Function property
-class Function:
-	var door: String # Can be "None", "Open" or "Closed"
-	var is_container: bool
-	var container_group: String
+# Inner class to handle the Craft property
+class Craft:
+	var craft_amount: int
+	var craft_time: int
+	var flags: Dictionary
+	var required_resources: Array
+	var skill_progression: Dictionary
+	var skill_requirement: Dictionary
 
-	# Constructor to initialize function properties from a dictionary
+	# Constructor to initialize craft properties from a dictionary
 	func _init(data: Dictionary):
-		door = data.get("door", "None")
-		is_container = data.get("is_container", false)
-		container_group = data.get("container_group", "")
+		craft_amount = data.get("craft_amount", 1)
+		craft_time = data.get("craft_time", 0)
+		flags = data.get("flags", {})
+		required_resources = data.get("required_resources", [])
+		skill_progression = data.get("skill_progression", {})
+		skill_requirement = data.get("skill_requirement", {})
 
 	# Get data function to return a dictionary with all properties
 	func get_data() -> Dictionary:
-		var functiondata: Dictionary = {}
-		if is_container:
-			functiondata["is_container"] = is_container
-			if not container_group == "":
-				functiondata["container_group"] = container_group
-		if not door == "None":
-			functiondata["door"] = door
-		return functiondata # Potentially return an empty dictionary
-
-
-# Inner class to handle the Support Shape property
-class SupportShape:
-	var color: String
-	var depth_scale: float
-	var height: float
-	var shape: String
-	var transparent: bool
-	var width_scale: float
-	var radius_scale: float
-
-	# Constructor to initialize support shape properties from a dictionary
-	func _init(data: Dictionary):
-		set_data(data)
-
-	func set_data(data: Dictionary):
-		color = data.get("color", "ffffffff")
-		depth_scale = data.get("depth_scale", 100.0)
-		height = data.get("height", 0.5)
-		shape = data.get("shape", "Box")
-		transparent = data.get("transparent", false)
-		width_scale = data.get("width_scale", 100.0)
-		radius_scale = data.get("radius_scale", 100.0)
-
-	# Get data function to return a dictionary with all properties
-	func get_data() -> Dictionary:
-		var shapedata: Dictionary = {
-			"color": color,
-			"height": height,
-			"shape": shape,
-			"transparent": transparent
+		return {
+			"craft_amount": craft_amount,
+			"craft_time": craft_time,
+			"flags": flags,
+			"required_resources": required_resources,
+			"skill_progression": skill_progression,
+			"skill_requirement": skill_requirement
 		}
-		if shape == "Box":
-			shapedata["width_scale"] = width_scale
-			shapedata["depth_scale"] = depth_scale
-		elif shape == "Cylinder":
-			shapedata["radius_scale"] = radius_scale
-		return shapedata
 
+# Inner class to handle the Magazine property
+class Magazine:
+	var current_ammo: int
+	var max_ammo: int
+	var used_ammo: String
 
-# Inner class to handle the Destruction property
-class Destruction:
-	var group: String
-	var sprite: String
-
-	# Constructor to initialize destruction properties from a dictionary
+	# Constructor to initialize magazine properties from a dictionary
 	func _init(data: Dictionary):
-		group = data.get("group", "")
-		sprite = data.get("sprite", "")
+		current_ammo = data.get("current_ammo", 0)
+		max_ammo = data.get("max_ammo", 0)
+		used_ammo = data.get("used_ammo", "")
 
 	# Get data function to return a dictionary with all properties
 	func get_data() -> Dictionary:
-		var destructiondata: Dictionary = {}
-		if not group == "":
-			destructiondata["group"] = group
-		if not sprite == "":
-			destructiondata["sprite"] = sprite
-		return destructiondata # Potentially return an empty dictionary
+		return {
+			"current_ammo": current_ammo,
+			"max_ammo": max_ammo,
+			"used_ammo": used_ammo
+		}
 
 
-# Inner class to handle the Disassembly property
-class Disassembly:
-	var group: String
-	var sprite: String
+# Inner class to handle the Ranged property
+class Ranged:
+	var firing_speed: float
+	var range: int
+	var recoil: int
+	var reload_speed: float
+	var spread: int
+	var sway: int
+	var used_ammo: String
+	var used_magazine: String
+	var used_skill: Dictionary
 
-	# Constructor to initialize disassembly properties from a dictionary
+	# Constructor to initialize ranged properties from a dictionary
 	func _init(data: Dictionary):
-		group = data.get("group", "")
-		sprite = data.get("sprite", "")
+		firing_speed = data.get("firing_speed", 0.0)
+		range = data.get("range", 0)
+		recoil = data.get("recoil", 0)
+		reload_speed = data.get("reload_speed", 0.0)
+		spread = data.get("spread", 0)
+		sway = data.get("sway", 0)
+		used_ammo = data.get("used_ammo", "")
+		used_magazine = data.get("used_magazine", "")
+		used_skill = data.get("used_skill", {})
 
 	# Get data function to return a dictionary with all properties
 	func get_data() -> Dictionary:
-		var disassemblydata: Dictionary = {}
-		if not group == "":
-			disassemblydata["group"] = group
-		if not sprite == "":
-			disassemblydata["sprite"] = sprite
-		return disassemblydata # Potentially return an empty dictionary
+		return {
+			"firing_speed": firing_speed,
+			"range": range,
+			"recoil": recoil,
+			"reload_speed": reload_speed,
+			"spread": spread,
+			"sway": sway,
+			"used_ammo": used_ammo,
+			"used_magazine": used_magazine,
+			"used_skill": used_skill
+		}
 
+
+# Inner class to handle the Melee property
+class Melee:
+	var damage: int
+	var reach: int
+	var used_skill: Dictionary
+
+	# Constructor to initialize melee properties from a dictionary
+	func _init(data: Dictionary):
+		damage = data.get("damage", 0)
+		reach = data.get("reach", 0)
+		used_skill = data.get("used_skill", {})
+
+	# Get data function to return a dictionary with all properties
+	func get_data() -> Dictionary:
+		return {
+			"damage": damage,
+			"reach": reach,
+			"used_skill": used_skill
+		}
+
+# Inner class to handle the Food property
+class Food:
+	var health: int
+
+	# Constructor to initialize food properties from a dictionary
+	func _init(data: Dictionary):
+		health = data.get("health", 0)
+
+	# Get data function to return a dictionary with all properties
+	func get_data() -> Dictionary:
+		return {
+			"health": health
+		}
+
+# Inner class to handle the Wearable property
+class Wearable:
+	var slot: String
+
+	# Constructor to initialize wearable properties from a dictionary
+	func _init(data: Dictionary):
+		slot = data.get("slot", "")
+
+	# Get data function to return a dictionary with all properties
+	func get_data() -> Dictionary:
+		return {
+			"slot": slot
+		}
 
 # Constructor to initialize item properties from a dictionary
 func _init(data: Dictionary):
-	id = data.get("id", 0)
+	id = data.get("id", "")
 	name = data.get("name", "")
 	description = data.get("description", "")
-	categories = data.get("categories", [])
-	moveable = data.get("moveable", false)
-	weight = data.get("weight", 1.0)
-	edgesnapping = data.get("edgesnapping", "")
+	weight = data.get("weight", 0.0)
+	volume = data.get("volume", 0.0)
 	spriteid = data.get("sprite", "")
-	function = Function.new(data.get("Function", {}))  # Initialize Function inner class
-	support_shape = SupportShape.new(data.get("support_shape", {}))  # Initialize SupportShape inner class
-	destruction = Destruction.new(data.get("destruction", {}))  # Initialize Destruction inner class
-	disassembly = Disassembly.new(data.get("disassembly", {}))  # Initialize Disassembly inner class
+	image = data.get("image", "")
+	stack_size = data.get("stack_size", 0)
+	max_stack_size = data.get("max_stack_size", 0)
+	two_handed = data.get("two_handed", false)
 	references = data.get("references", {})
+
+	# Initialize Craft and Magazine subclasses if they exist in data
+	if data.has("Craft"):
+		craft = Craft.new(data["Craft"][0])
+	else:
+		craft = null
+
+	if data.has("Magazine"):
+		magazine = Magazine.new(data["Magazine"])
+	else:
+		magazine = null
+
+	if data.has("Ranged"):
+		ranged = Ranged.new(data["Ranged"])
+	else:
+		ranged = null
+
+	if data.has("Melee"):
+		melee = Melee.new(data["Melee"])
+	else:
+		melee = null
+
+	if data.has("Food"):
+		food = Food.new(data["Food"])
+	else:
+		food = null
+
+	if data.has("Wearable"):
+		wearable = Wearable.new(data["Wearable"])
+	else:
+		wearable = null
 
 
 # Get data function to return a dictionary with all properties
@@ -148,30 +216,35 @@ func get_data() -> Dictionary:
 		"id": id,
 		"name": name,
 		"description": description,
-		"categories": categories,
-		"moveable": moveable,
 		"weight": weight,
-		"edgesnapping": edgesnapping,
+		"volume": volume,
 		"sprite": spriteid,
+		"image": image,
+		"stack_size": stack_size,
+		"max_stack_size": max_stack_size,
+		"two_handed": two_handed,
 		"references": references
 	}
-	# Save the weight only if moveable true, otherwise erase it.
-	if moveable:
-		data["weight"] = weight
-	else: # Support shape only applies to static item
-		data["support_shape"] = support_shape.get_data()
-		
-	var functiondata: Dictionary = function.get_data()
-	if not functiondata.is_empty():
-		data["Function"] = functiondata
-		
-	var destructiondata: Dictionary = destruction.get_data()
-	if not destructiondata.is_empty():
-		data["destruction"] = destructiondata
-		
-	var disassemblydata: Dictionary = disassembly.get_data()
-	if not disassemblydata.is_empty():
-		data["disassembly"] = disassemblydata
+
+	# Add Craft and Magazine data if they exist
+	if craft:
+		data["Craft"] = [craft.get_data()]
+
+	if magazine:
+		data["Magazine"] = magazine.get_data()
+
+	if ranged:
+		data["Ranged"] = ranged.get_data()
+
+	if melee:
+		data["Melee"] = melee.get_data()
+
+	if food:
+		data["Food"] = food.get_data()
+
+	if wearable:
+		data["Wearable"] = wearable.get_data()
+
 	return data
 
 
@@ -204,20 +277,7 @@ func get_sprite_path() -> String:
 
 # Handles item changes and updates references if necessary
 func on_data_changed(oldditem: DItem):
-	var old_container_group = oldditem.function.container_group
-	var new_container_group = function.container_group
-	var old_destruction_group = oldditem.destruction.group
-	var old_disassembly_group = oldditem.disassembly.group
 	var changes_made = false
-
-	# Handle container itemgroup changes
-	changes_made = Gamedata.update_reference(old_container_group, new_container_group, id, "item") or changes_made
-
-	# Handle destruction group changes
-	changes_made = Gamedata.update_reference(old_destruction_group, destruction.group, id, "item") or changes_made
-
-	# Handle disassembly group changes
-	changes_made = Gamedata.update_reference(old_disassembly_group, disassembly.group, id, "item") or changes_made
 
 	# If any references were updated, save the changes to the data file
 	if changes_made:
@@ -229,25 +289,8 @@ func on_data_changed(oldditem: DItem):
 # We have to remove it from everything that references it
 func delete():
 	var changes_made = false
-	var itemgroup = function.container.get("itemgroup", "")
-	changes_made = Gamedata.dremove_reference(references, "core", "item", itemgroup) or changes_made
-	changes_made = Gamedata.dremove_reference(references, "core", "item", destruction.group) or changes_made
-	changes_made = Gamedata.dremove_reference(references, "core", "item", disassembly.group) or changes_made
-	
-	var mapsdata = Helper.json_helper.get_nested_data(references, "core.maps")
-	Gamedata.maps.remove_entity_from_selected_maps("item", id, mapsdata)
 	if changes_made:
 		Gamedata.save_data_to_file(Gamedata.data.itemgroups)
 	else:
 		print_debug("No changes needed for item", id)
 
-
-# Removes any instance of an itemgroup from the item
-func remove_itemgroup(itemgroup_id: String) -> void:
-	if function.container_group == itemgroup_id:
-		function.container_group = ""
-	if destruction.group == itemgroup_id:
-		destruction.group = ""
-	if disassembly.group == itemgroup_id:
-		disassembly.group = ""
-	Gamedata.items.save_items_to_disk()
