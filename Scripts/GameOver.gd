1e0423b1a92ba17af394457feb86e45f49156317
snipercup <50166150+snipercup@users.noreply.github.com>
2024-07-26 13:38:18 +0200

    Return to main menu after game over

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -5,6 +5,9 @@ extends Control
 
 # When the player presses the 'return to main menu' button
 func _on_return_button_button_up():
-	await Helper.save_helper.all_chunks_unloaded
+	Helper.map_manager.level_generator.unload_all_chunks()
+	await Helper.map_manager.level_generator.all_chunks_unloaded
+	# Devides the loaded_chunk_data.chunks into segments and saves them to disk
+	Helper.overmap_manager.unload_all_remaining_segments()
 	Helper.signal_broker.game_ended.emit()
 	get_tree().change_scene_to_file("res://scene_selector.tscn")
