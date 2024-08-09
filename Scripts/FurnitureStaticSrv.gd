cf4a7a5d04242038c67f094de0b81b6d3ca765e6
snipercup <50166150+snipercup@users.noreply.github.com>
2024-08-09 23:08:12 +0200

    Correct position and height

--- Scripts/FurnitureStaticSrv.gd
+++ Scripts/FurnitureStaticSrv.gd
@@ -2,6 +2,7 @@ class_name FurnitureStaticSrv
 extends Node3D # Has to be Node3D. Changing it to RefCounted doesn't work
 
 # Variables to store furniture data
+var furniture_transform: FurnitureTransform
 var furniture_position: Vector3
 var furniture_rotation: int
 var furnitureJSON: Dictionary # The json that defines this furniture on the map
@@ -17,67 +18,193 @@ var support_mesh: PrimitiveMesh
 var sprite_texture: Texture2D  # Variable to store the sprite texture
 var quad_mesh: PlaneMesh
 
+class FurnitureTransform:
+	var posx: float
+	var posy: float
+	var posz: float
+	var rot: int
+	var width: float
+	var depth: float
+	var height: float
+
+	func _init(myposition: Vector3, myrotation: int, size: Vector3):
+		posx = myposition.x
+		posy = myposition.y
+		posz = myposition.z
+		rot = myrotation
+		width = size.x
+		depth = size.z
+		height = size.y
+
+	func get_position() -> Vector3:
+		return Vector3(posx, posy, posz)
+
+	func set_position(new_position: Vector3):
+		posx = new_position.x
+		posy = new_position.y
+		posz = new_position.z
+
+	func get_rotation() -> int:
+		return rot
+
+	func set_rotation(new_rotation: int):
+		rot = new_rotation
+
+	func get_sizeV3() -> Vector3:
+		return Vector3(width, height, depth)
+
+	func get_sizeV2() -> Vector2:
+		return Vector2(width, depth)
+
+	func set_size(new_size: Vector3):
+		width = new_size.x
+		height = new_size.y
+		depth = new_size.z
+
+	func update_transform(new_position: Vector3, new_rotation: int, new_size: Vector3):
+		set_position(new_position)
+		set_rotation(new_rotation)
+		set_size(new_size)
+	
+	# New method to create a Transform3D
+	func get_sprite_transform() -> Transform3D:
+		var adjusted_position = get_position() + Vector3(0, 0.5+(1*height)+0.01, 0)
+		return Transform3D(Basis(Vector3(0, 1, 0), deg_to_rad(rot)), adjusted_position)
+	
+	func get_cylinder_shape_data() -> Dictionary:
+		return {"radius": width / 4.0, "height": height}
+	
+	# New method to create a Transform3D for visual instances
+	func get_visual_transform() -> Transform3D:
+		var adjusted_position = get_position() + Vector3(0, 0.5+(0.5*height), 0)
+		return Transform3D(Basis(Vector3(0, 1, 0), deg_to_rad(rot)), adjusted_position)
+	
+	func get_box_shape_size() -> Vector3:
+		return Vector3(width / 2.0, height / 2.0, depth / 2.0)
+
+
 # Function to initialize the furniture object
 func _init(furniturepos: Vector3, newFurnitureJSON: Dictionary, world3d: World3D):
 	furniture_position = furniturepos
+	print_debug("furniture_position = ", furniture_position)
 	furnitureJSON = newFurnitureJSON
 	furniture_rotation = furnitureJSON.get("rotation", 0)
 	dfurniture = Gamedata.furnitures.by_id(furnitureJSON.id)
 	myworld3d = world3d
 
 	sprite_texture = dfurniture.sprite
-	var sprite_size = calculate_sprite_size()
+	var furniture_size: Vector3 = calculate_furniture_size()
+	
+	furniture_transform = FurnitureTransform.new(furniturepos, furniture_rotation, furniture_size)
 
 	if is_new_furniture():
-		furniture_position.y += 0.525 # Move the furniture to slightly above the block
-		apply_edge_snapping_if_needed(sprite_size)
+		#furniture_position.y += 0.1 # Move the furniture to slightly above the block
+		apply_edge_snapping_if_needed()
 
 	set_new_rotation(furniture_rotation) # Apply rotation after setting up the shape and visual instance
 	if dfurniture.support_shape.shape == "Box":
-		create_box_shape(sprite_size)
-		create_visual_instance(sprite_size, "Box")
+		create_box_shape()
+		create_visual_instance("Box")
 	elif dfurniture.support_shape.shape == "Cylinder":
-		create_cylinder_shape(Vector2(sprite_size.x, sprite_size.z))
-		create_visual_instance(Vector3(sprite_size.x, dfurniture.support_shape.height, sprite_size.z), "Cylinder")
+		create_cylinder_shape()
+		create_visual_instance("Cylinder")
 
-	create_sprite_instance(sprite_size)
+	create_sprite_instance()
 
 
-# Function to calculate the size of the sprite
-func calculate_sprite_size() -> Vector3:
+# Function to calculate the size of the furniture
+func calculate_furniture_size() -> Vector3:
 	if sprite_texture:
 		var sprite_width = sprite_texture.get_width() / 100.0 # Convert pixels to meters
-		var sprite_height = sprite_texture.get_height() / 100.0 # Convert pixels to meters
-		return Vector3(sprite_width, 0.5, sprite_height)  # Default height of 0.5 meters
-	return Vector3(0.5, 0.5, 0.5)  # Default size if texture is not set
+		var sprite_depth = sprite_texture.get_height() / 100.0 # Convert pixels to meters
+		var height = dfurniture.support_shape.height
+		return Vector3(sprite_width, height, sprite_depth)  # Use height from support shape
+	return Vector3(0.5, dfurniture.support_shape.height, 0.5)  # Default size if texture is not set
 
 
 # Function to create a BoxShape3D collider based on the given size
-func create_box_shape(size: Vector3):
+func create_box_shape():
 	shape = PhysicsServer3D.box_shape_create()
-	PhysicsServer3D.shape_set_data(shape, Vector3(size.x / 2.0, size.y / 2.0, size.z / 2.0))
+	PhysicsServer3D.shape_set_data(shape, furniture_transform.get_box_shape_size())
 	
 	collider = PhysicsServer3D.body_create()
 	PhysicsServer3D.body_set_mode(collider, PhysicsServer3D.BODY_MODE_STATIC)
 	# Set space, so it collides in the same space as current scene.
 	PhysicsServer3D.body_set_space(collider, myworld3d.space)
 	PhysicsServer3D.body_add_shape(collider, shape)
-	# Apply rotation to the transform
-	var mytransform = Transform3D(Basis(Vector3(0, 1, 0), deg_to_rad(furniture_rotation)), furniture_position)
+
+	var mytransform = furniture_transform.get_visual_transform()
 	PhysicsServer3D.body_set_state(collider, PhysicsServer3D.BODY_STATE_TRANSFORM, mytransform)
 	set_collision_layers_and_masks()
 
+
+# Function to create a visual instance with a mesh to represent the shape
+func create_visual_instance(shape_type: String):
+	var color = Color.html(dfurniture.support_shape.color)
+	var material: StandardMaterial3D = StandardMaterial3D.new()
+	material.albedo_color = color
+	if dfurniture.support_shape.transparent:
+		material.flags_transparent = true
+		material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	
+	if shape_type == "Box":
+		support_mesh = BoxMesh.new()
+		(support_mesh as BoxMesh).size = furniture_transform.get_sizeV3()
+	elif shape_type == "Cylinder":
+		support_mesh = CylinderMesh.new()
+		(support_mesh as CylinderMesh).height = furniture_transform.height
+		(support_mesh as CylinderMesh).top_radius = furniture_transform.width / 4.0
+		(support_mesh as CylinderMesh).bottom_radius = furniture_transform.width / 4.0
+
+	support_mesh.material = material
+
+	mesh_instance = RenderingServer.instance_create()
+	RenderingServer.instance_set_base(mesh_instance, support_mesh)
+	
+	RenderingServer.instance_set_scenario(mesh_instance, myworld3d.scenario)
+	var mytransform = furniture_transform.get_visual_transform()
+	RenderingServer.instance_set_transform(mesh_instance, mytransform)
+
+
+# Function to create a QuadMesh to display the sprite texture on top of the furniture
+func create_sprite_instance():
+	quad_mesh = PlaneMesh.new()
+	quad_mesh.size = furniture_transform.get_sizeV2()
+	var material = StandardMaterial3D.new()
+	material.albedo_texture = sprite_texture
+	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	#material.flags_unshaded = true  # Optional: make the sprite unshaded
+	quad_mesh.material = material
+	
+	var quad_instance = RenderingServer.instance_create()
+	RenderingServer.instance_set_base(quad_instance, quad_mesh)
+	RenderingServer.instance_set_scenario(quad_instance, myworld3d.scenario)
+	
+	# Set the transform for the quad instance to be slightly above the box mesh
+	RenderingServer.instance_set_transform(quad_instance, furniture_transform.get_sprite_transform())
+
+
+# Now, update methods that involve position, rotation, and size
+func apply_edge_snapping_if_needed():
+	if not dfurniture.edgesnapping == "None":
+		var new_position = apply_edge_snapping(
+			dfurniture.edgesnapping
+		)
+		furniture_transform.set_position(new_position)
+
+
 # Function to create a CylinderShape3D collider based on the given size
-func create_cylinder_shape(size: Vector2):
+func create_cylinder_shape():
 	shape = PhysicsServer3D.cylinder_shape_create()
-	PhysicsServer3D.shape_set_data(shape, {"radius": size.x / 4.0, "height": dfurniture.support_shape.height})
+	PhysicsServer3D.shape_set_data(shape, furniture_transform.get_cylinder_shape_data())
 	
 	collider = PhysicsServer3D.body_create()
 	PhysicsServer3D.body_set_mode(collider, PhysicsServer3D.BODY_MODE_STATIC)
 	# Set space, so it collides in the same space as current scene.
 	PhysicsServer3D.body_set_space(collider, myworld3d.space)
 	PhysicsServer3D.body_add_shape(collider, shape)
-	PhysicsServer3D.body_set_state(collider, PhysicsServer3D.BODY_STATE_TRANSFORM, Transform3D(Basis(), furniture_position))
+	var mytransform = furniture_transform.get_visual_transform()
+	PhysicsServer3D.body_set_state(collider, PhysicsServer3D.BODY_STATE_TRANSFORM, mytransform)
 	set_collision_layers_and_masks()
 
 
@@ -100,69 +227,14 @@ func set_collision_layers_and_masks():
 	PhysicsServer3D.body_set_collision_mask(collider, collision_mask)
 
 
-# Function to create a visual instance with a mesh to represent the shape
-func create_visual_instance(size: Vector3, shape_type: String):
-	var color = Color.html(dfurniture.support_shape.color)
-	var material: StandardMaterial3D = StandardMaterial3D.new()
-	material.albedo_color = color
-	if dfurniture.support_shape.transparent:
-		material.flags_transparent = true
-		material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
-	
-	if shape_type == "Box":
-		support_mesh = BoxMesh.new()
-		(support_mesh as BoxMesh).size = size
-	elif shape_type == "Cylinder":
-		support_mesh = CylinderMesh.new()
-		(support_mesh as CylinderMesh).height = size.y
-		(support_mesh as CylinderMesh).top_radius = size.x / 4.0
-		(support_mesh as CylinderMesh).bottom_radius = size.z / 4.0
-
-	support_mesh.material = material
-
-	mesh_instance = RenderingServer.instance_create()
-	RenderingServer.instance_set_base(mesh_instance, support_mesh)
-	
-	RenderingServer.instance_set_scenario(mesh_instance, myworld3d.scenario)
-	# Apply rotation to the transform
-	var mytransform = Transform3D(Basis(Vector3(0, 1, 0), deg_to_rad(furniture_rotation)), furniture_position)
-	RenderingServer.instance_set_transform(mesh_instance, mytransform)
-
-
-# Function to create a QuadMesh to display the sprite texture on top of the furniture
-func create_sprite_instance(size: Vector3):
-	quad_mesh = PlaneMesh.new()
-	quad_mesh.size = Vector2(size.x, size.z)
-	var material = StandardMaterial3D.new()
-	material.albedo_texture = sprite_texture
-	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
-	#material.flags_unshaded = true  # Optional: make the sprite unshaded
-	quad_mesh.material = material
-	
-	var quad_instance = RenderingServer.instance_create()
-	RenderingServer.instance_set_base(quad_instance, quad_mesh)
-	RenderingServer.instance_set_scenario(quad_instance, myworld3d.scenario)
-	
-	# Set the transform for the quad instance to be slightly above the box mesh
-	var mytransform = Transform3D(Basis(Vector3(0, 1, 0), deg_to_rad(furniture_rotation)), furniture_position + Vector3(0, 0.51, 0))  # Adjust the Y position as needed
-	RenderingServer.instance_set_transform(quad_instance, mytransform)
-
-
-# Function to apply edge snapping if necessary
-func apply_edge_snapping_if_needed(size: Vector3):
-	if not dfurniture.edgesnapping == "None":
-		furniture_position = apply_edge_snapping(
-			furniture_position, dfurniture.edgesnapping, 
-			size, furniture_rotation, furniture_position
-		)
-
-
 # If edge snapping has been set in the furniture editor, we will apply it here.
 # The direction refers to the 'backside' of the furniture, which will be facing the edge of the block
 # This is needed to put furniture against the wall, or get a fence at the right edge
-func apply_edge_snapping(newpos: Vector3, direction: String, size: Vector3, newRot: int, furniturepos: Vector3) -> Vector3:
+func apply_edge_snapping(direction: String) -> Vector3:
 	# Block size, a block is 1x1 meters
 	var blockSize = Vector3(1.0, 1.0, 1.0)
+	var newpos = furniture_transform.get_position()
+	var size: Vector3 = furniture_transform.get_sizeV3()
 	
 	# Adjust position based on edgesnapping direction and rotation
 	match direction:
@@ -177,15 +249,15 @@ func apply_edge_snapping(newpos: Vector3, direction: String, size: Vector3, newR
 		# Add more cases if needed
 	
 	# Consider rotation if necessary
-	newpos = rotate_position_around_block_center(newpos, newRot, furniturepos)
+	newpos = rotate_position_around_block_center(newpos, furniture_transform.get_position())
 	
 	return newpos
 
 
 # Called when applying edge-snapping so it's put into the right position
-func rotate_position_around_block_center(newpos: Vector3, newRot: int, block_center: Vector3) -> Vector3:
+func rotate_position_around_block_center(newpos: Vector3, block_center: Vector3) -> Vector3:
 	# Convert rotation to radians for trigonometric functions
-	var radians = deg_to_rad(newRot)
+	var radians = deg_to_rad(furniture_transform.get_rotation())
 	
 	# Calculate the offset from the block center
 	var offset = newpos - block_center
@@ -208,12 +280,12 @@ func set_new_rotation(amount: int):
 		rotation_amount = amount - 180
 	elif amount == 90:
 		rotation_amount = amount + 180
-	furniture_rotation = rotation_amount
+	furniture_transform.set_rotation(rotation_amount)
 
 
 # Function to get the current rotation of this furniture
 func get_my_rotation() -> int:
-	return furniture_rotation
+	return furniture_transform.get_rotation()
 
 
 # Helper function to determine if the furniture is new
