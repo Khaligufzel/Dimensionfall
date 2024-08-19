4fa242614620ae29e66f56e2b184087455380156
snipercup <50166150+snipercup@users.noreply.github.com>
2024-08-20 00:43:27 +0200

    Correct brush preview texture position

--- Scenes/ContentManager/Mapeditor/Scripts/GridContainer.gd
+++ Scenes/ContentManager/Mapeditor/Scripts/GridContainer.gd
@@ -162,37 +162,43 @@ func _input(event) -> void:
 		_update_brush_preview_position()
 
 
-# Function to update the position of the brush preview based on the mouse event
+# Function to calculate the pivot offset based on rotation
+func calculate_brush_offset() -> Vector2:
+	var scaled_size = brushPreviewTexture.size * brushPreviewTexture.scale
+	match rotationAmount:
+		90:
+			return Vector2(scaled_size.x, 0)
+		180:
+			return Vector2(scaled_size.x, scaled_size.y)
+		270:
+			return Vector2(0, scaled_size.y)
+		_:
+			return Vector2(0, 0)
+
+
 func _update_brush_preview_position() -> void:
-	# Scale the brush preview based on the map editor's zoom level
+	# Get the current scale factor based on the zoom level
 	var scale_factor = mapEditor.zoom_level * 0.01
 	brushPreviewTexture.scale = Vector2(scale_factor, scale_factor)
-
-	# Calculate the pivot offset for centering the preview
-	brushPreviewTexture.pivot_offset = calculate_scaled_center_distance()
-
-	# Get the current mouse position in the viewport
+	
+	# Calculate the scaled size of the brushPreviewTexture
+	var scaled_size = brushPreviewTexture.size * scale_factor
+	
+	# Get the current mouse position
 	var mouse_position = get_viewport().get_mouse_position()
 	
-	# Define a constant offset to slightly offset the preview from the cursor
-	var constant_offset = Vector2(10, 10)
+	# Calculate the new position for the brushPreviewTexture
+	var new_position = mouse_position - (scaled_size / 2) + scaled_size
 	
-	# Calculate the difference due to scaling
-	var scalediff = brushPreviewTexture.size - brushPreviewTexture.size * brushPreviewTexture.scale.x
-	
-	# Calculate the new position for the brush preview
-	var new_position = mouse_position + constant_offset - scalediff / 2
-	
-	# Get the global position and size of the scroll window
+	# Ensure the brushPreviewTexture stays within the bounds of the mapScrollWindow
 	var scroll_global_pos = mapScrollWindow.get_global_position()
 	var mapScrollWindowRect = mapScrollWindow.get_rect()
-	
-	# Clamp the new position to keep it within the bounds of the scroll window
 	new_position.x = clamp(new_position.x, scroll_global_pos.x, scroll_global_pos.x + mapScrollWindowRect.size.x - brushPreviewTexture.get_rect().size.x)
 	new_position.y = clamp(new_position.y, scroll_global_pos.y, scroll_global_pos.y + mapScrollWindowRect.size.y - brushPreviewTexture.get_rect().size.y)
-	
-	# Apply the calculated position to the brush preview texture
+	new_position += calculate_brush_offset()
+	# Set the position of the brushPreviewTexture
 	brushPreviewTexture.global_position = new_position
+
 
 
 func _on_zoom_level_changed(zoom_level: int):
