5ab251cf42a74ef9ca69b5d6a5e7f4cc49cfb8da
snipercup <50166150+snipercup@users.noreply.github.com>
2024-10-04 09:40:08 +0200

    Add gaea addon, remove kenney

---
+++ addons/gaea/generators/2D/wave_function_generator/wave_function_generator_2d_settings.gd
@@ -0,0 +1,6 @@
+@tool
+class_name WaveFunctionGenerator2DSettings
+extends GeneratorSettings2D
+
+@export var world_size: Vector2i = Vector2i(32, 32)
+@export var entries: Array[WaveFunction2DEntry]
---
+++ addons/gaea/tile_info/gridmap_tile_info.gd
@@ -0,0 +1,4 @@
+class_name GridmapTileInfo
+extends TileInfo
+
+@export var index: int
