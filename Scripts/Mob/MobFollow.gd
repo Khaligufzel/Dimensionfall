4fad2b12b5e30763a2a2056872e4cf096c433b83
snipercup <50166150+snipercup@users.noreply.github.com>
2024-11-05 19:59:46 +0100

    Refactor

--- Scripts/Mob/MobFollow.gd
+++ Scripts/Mob/MobFollow.gd
@@ -47,38 +47,60 @@ func Exit():
 # transitioning to an attack state if within melee range.
 func Physics_Update(_delta: float):
 	if mob.terminated:
-		Transistioned.emit(self, "mobterminate") 
+		Transistioned.emit(self, "mobterminate")
+		return
 	
+	move_toward_target()
+	orient_toward_target()
+	check_for_player_in_range()
+	check_if_idle()
+
+
+# Moves the mob towards the next position in the navigation path
+func move_toward_target():
 	var next_pos: Vector3 = nav_agent.get_next_path_position()
 	if nav_agent.get_navigation_map() == null or next_pos == mob.global_transform.origin:
 		var current_chunk = mob.get_chunk_from_position(target_location)
 		mob.update_navigation_agent_map(current_chunk)
 		return
+	
 	var dir = mob.to_local(next_pos).normalized()
 	mob.velocity = dir * mob.current_move_speed
 	mob.move_and_slide()
 
-	# Rotation towards target using look_at
+
+# Orients the mob to face the targeted player, aligning y-axis to prevent tilting
+func orient_toward_target():
 	if targeted_player:
 		var target_position = targeted_player.global_position
 		target_position.y = mob.meshInstance.global_position.y  # Align y-axis to avoid tilting
 		mob.meshInstance.look_at(target_position, Vector3.UP)
-	
+
+
+# Performs raycasting to check if the targeted player is within sight and melee range
+func check_for_player_in_range():
 	if !targeted_player:
 		return
+	
 	var space_state = get_world_3d().direct_space_state
 	# TODO Change playerCol to group of players
-	var query = PhysicsRayQueryParameters3D.create(mobCol.global_position, targeted_player.global_position, int(pow(2, 1-1) + pow(2, 3-1)),[self])
+	var query = PhysicsRayQueryParameters3D.create(
+		mobCol.global_position,
+		targeted_player.global_position,
+		int(pow(2, 1 - 1) + pow(2, 3 - 1)), # Testing only for collision layers 1 and 3 
+		[self] # Exclude self
+	)
 	var result = space_state.intersect_ray(query)
-	
 	if result and result.collider:
-		
-		if result.collider.is_in_group("Players")&& Vector3(mobCol.global_position).distance_to(targeted_player.global_position) <= mob.melee_range / 2:
+		if result.collider.is_in_group("Players") and Vector3(mobCol.global_position).distance_to(targeted_player.global_position) <= mob.melee_range / 2:
 			print("changing state to mobattack...")
 			Transistioned.emit(self, "mobattack")
-	
+
+
+# Checks if the mob has reached its target location, transitions to idle if true
+func check_if_idle():
 	if Vector3(mob.global_position).distance_to(target_location) <= 0.5:
-		Transistioned.emit(self, "mobidle") 
+		Transistioned.emit(self, "mobidle")
 
 
 # Sets the target position for the navigation agent based on target location.
