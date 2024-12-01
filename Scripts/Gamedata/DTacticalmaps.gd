a003fba5345c6bde42c3b06a413bc4aadfa9d6c8
snipercup <50166150+snipercup@users.noreply.github.com>
2024-12-01 18:15:25 +0100

    Add new tacticalmap to dict

--- Scripts/Gamedata/DTacticalmaps.gd
+++ Scripts/Gamedata/DTacticalmaps.gd
@@ -34,6 +34,7 @@ func duplicate_to_disk(mapid: String, newmapid: String) -> void:
 func add_new(newid: String) -> void:
 	var newmap: DTacticalmap = DTacticalmap.new(newid, dataPath)
 	newmap.save_data_to_disk()
+	mapdict[newid] = newmap
 
 func delete_by_id(mapid: String) -> void:
 	mapdict[mapid].delete()
