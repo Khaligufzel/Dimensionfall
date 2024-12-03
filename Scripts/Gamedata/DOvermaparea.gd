40c4fc71dfc7681cf54f3ba856419744b8c2aaeb
snipercup <50166150+snipercup@users.noreply.github.com>
2024-12-03 11:46:48 +0100

    Add overmap area reference

--- Scripts/Gamedata/DOvermaparea.gd
+++ Scripts/Gamedata/DOvermaparea.gd
@@ -193,11 +193,11 @@ func changed(olddata: DOvermaparea):
 	# Remove references to map IDs that are in the old data but not in the new data
 	for map_id in old_map_ids:
 		if map_id not in new_map_ids:
-			Gamedata.mods.by_id("Core").maps.remove_reference_from_map(map_id, "core", "overmapareas", id)
+			Gamedata.mods.remove_reference(DMod.ContentType.MAPS, map_id, DMod.ContentType.OVERMAPAREAS, id)
 
 	# Add references to map IDs that are in the new data, even if they were already in the old data
 	for map_id in new_map_ids:
-		Gamedata.mods.by_id("Core").maps.add_reference_to_map(map_id, "core", "overmapareas", id)
+		Gamedata.mods.add_reference(DMod.ContentType.MAPS, map_id, DMod.ContentType.OVERMAPAREAS, id)
 
 	# Save the updated overmap area data to disk
 	Gamedata.overmapareas.save_overmapareas_to_disk()
