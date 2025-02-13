108267e958ae956340d6f6d91b810d8db11a4f9b
Diamond-Dan <dreti.biz@gmail.com>
2025-02-13 10:18:38 -0800

    Added addition tests
    Added addition tests to player test template

--- Tests/Unit/test_player.gd
+++ Tests/Unit/test_player.gd
@@ -3,8 +3,9 @@ var scene_selector
 var player
 
 
-#function to get player from scene
+#Basic unit test for player
 func before_all():
+	#load data required for scene, scene may need to be refactored to avoid this
 	Runtimedata.reconstruct() # Load all mod data in the proper way
 	var rng = RandomNumberGenerator.new()
 	Helper.mapseed = rng.randi()
@@ -13,26 +14,26 @@ func before_all():
 	scene_selector = preload("res://level_generation.tscn").instantiate()
 	get_tree().root.add_child(scene_selector)
 	await get_tree().process_frame 
-	#scene_selector = preload("res://scene_selector.tscn").instantiate()
-	#get_tree().root.add_child(scene_selector)
-	#await get_tree().process_frame 
-	#
-	#
-	#
-	#var playbut= scene_selector.get_node("PlayDemo") as Button
-	#playbut.emit_signal("pressed")
 	
-#test this function on the player	
-func test_initial_stm() -> void:
-	#scene_selector._on_play_demo_pressed()
-	gut.p(get_tree().get_current_scene().name)
+	
+#Function tests for the presence of a player
+func test_player_is_there()->void:
 	player = get_tree().get_nodes_in_group("Players") 
-	gut.p(player)
-	if player.size() > 0:
-		var player = player[0]
-		assert_eq(player.current_stamina, player.max_stamina, "Stamina is loading incorrectly!")
-	else:
-		gut.p("No player was found")
+	assert_eq(player.size(),1,"too many or not enough players")
+	
+	
+#Function tests is player is alive when spawned	and sets player to the first player array memeber
+func test_living()-> void:
+	player = player[0] #now that we know players are here set players to the only player
+	assert_true(player.is_alive,"Opps player spawned dead")
+
+#Function tests player stamina 	
+func test_initial_stm() -> void:
+	assert_eq(player.current_stamina, player.max_stamina, "Stamina is loading incorrectly!")
+
+func test_knockback()->void:
+	assert_false(player.knockback_active,"Player spawned with knockback error")#
+	
 
 
 	
