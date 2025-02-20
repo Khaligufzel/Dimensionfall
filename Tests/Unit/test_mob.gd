53d0a69788a9d2f333e3973e9bc91b30e47317d3
snipercup <50166150+snipercup@users.noreply.github.com>
2025-02-20 23:54:44 +0100

    Add melee combat test

--- Tests/Unit/test_mob.gd
+++ Tests/Unit/test_mob.gd
@@ -23,18 +23,10 @@ func before_each():
 
 	test_chunk.level_manager = mock_level_manager
 	test_chunk.level_generator = mock_level_generator
-
-	test_chunk.mypos = Vector3(32, 0, 64) # Example position (chunk (1, 2) with 32x32 blocks)
-	test_chunk.chunk_data = {
-		"id": "basic_mob_test_map",
-		"rotation": 0
-	}
-
 	add_child(mock_level_manager)
 	add_child(mock_level_generator)
-	add_child(test_chunk)
 
-	await get_tree().process_frame
+	test_chunk.mypos = Vector3(32, 0, 64) # Example position (chunk (1, 2) with 32x32 blocks)
 
 # Runs after each test.
 func after_each():
@@ -48,6 +40,9 @@ func after_each():
 	# Calls chunk_is_unloaded every second until it returns true and asserts on the returned value
 	assert_true(await wait_until(chunk_is_unloaded, 10, 1),"Chunk should be unloaded in 10 seconds")
 
+	var mobs: Array = get_tree().get_nodes_in_group("mobs") 
+	assert_eq(mobs.size(),0,"too many mobs")
+	
 	if test_chunk and is_instance_valid(test_chunk):
 		test_chunk.queue_free()
 	if mock_level_manager:
@@ -59,8 +54,17 @@ func after_each():
 func after_all():
 	Runtimedata.reset()
 
-# Test if the chunk initializes and spawns correctly.
+
+# Test if the chunk initializes and spawns and moves correctly.
 func test_mob_spawn():
+	test_chunk.chunk_data = {
+		"id": "basic_mob_test_map",
+		"rotation": 0
+	}
+
+	add_child(test_chunk)
+
+	await get_tree().process_frame
 	# Test if chunk state is set correctly
 	assert_eq(test_chunk.load_state, Chunk.LoadStates.LOADING, "Chunk did not start in LOADING state.")
 
@@ -70,9 +74,6 @@ func test_mob_spawn():
 	# Verify the state reset after generation
 	assert_eq(test_chunk.load_state, Chunk.LoadStates.NEITHER, "Chunk should have reset to NEITHER state after generation.")
 	
-	assert_eq(test_chunk.mypos, Vector3(32, 0, 64), "Chunk position is not set correctly.")
-	assert_eq(test_chunk.chunk_data["id"], "basic_mob_test_map", "Chunk data ID is not correct.")
-	
 	var mobs: Array = get_tree().get_nodes_in_group("mobs") 
 	assert_eq(mobs.size(),1,"too many or not enough mobs")
 	var first_mob: Mob = mobs[0]
@@ -80,7 +81,7 @@ func test_mob_spawn():
 	assert_eq(first_mob.mobPosition,Vector3(47,1.5,79),"Mob spawned somewhere else")
 	
 	# Test that the mob is in idle state, since there is no target
-	var current_state: State = first_mob.state_machine.current_state
+	var current_state: State = first_mob.get_current_state()
 	assert_is(current_state,MobIdle,"A different state then expected")
 	# Check that the mob has a NavigationAgent3D
 	assert_not_null(first_mob.nav_agent, "Mob's NavigationAgent3D is null.")
@@ -118,3 +119,49 @@ func test_mob_spawn():
 	var mob_has_moved = func():
 		return initial_position.distance_to(first_mob.last_position) > 0.01
 	assert_true(await wait_until(mob_has_moved, 10, 1),"Mob should have moved in 10 seconds")
+
+
+# Test if the chunk initializes and spawns and moves correctly.
+func test_mob_melee_combat():
+	test_chunk.chunk_data = {
+		"id": "melee_mob_combat_map",
+		"rotation": 0
+	}
+	add_child(test_chunk)
+
+	await get_tree().process_frame
+	# Wait for `chunk_generated` signal before verifying post-generation state
+	assert_true(await wait_for_signal(test_chunk.chunk_generated, 5), "Chunk should have emitted chunk_generated signal.")
+	
+	var mobs: Array = get_tree().get_nodes_in_group("mobs") 
+	assert_eq(mobs.size(),2,"too many or not enough mobs")
+	var first_mob: Mob = mobs[0]
+	assert_eq(first_mob.rmob.id,"generic_test_mob","A different mob spawned then expected")
+	assert_eq(first_mob.mobPosition,Vector3(46,1.5,79),"Mob spawned somewhere else")
+	var second_mob: Mob = mobs[1]
+	assert_eq(second_mob.rmob.id,"generic_enemy_mob","A different mob spawned then expected")
+	assert_eq(second_mob.mobPosition, Vector3(49, 1.5, 79), "Mob spawned somewhere else")
+
+	# Test that the mobs are moving and getting closer
+	var initial_distance: float = first_mob.global_position.distance_to(second_mob.global_position)
+	await wait_frames(30)
+	var new_distance: float = first_mob.global_position.distance_to(second_mob.global_position)
+	assert_true(
+		new_distance < initial_distance,
+		"Mobs did not get closer to each other. Initial distance: %s, New distance: %s" % [initial_distance, new_distance]
+	)
+	
+	# Test that the mob transitions into the mob attack state
+	var first_state: State = first_mob.get_current_state()
+	assert_not_null(first_state, "Mob has no state")
+	assert_true(await wait_for_signal(first_state.Transistioned, 5), "Mob doesn't transition")
+	first_state = first_mob.get_current_state()
+	assert_is(first_state,MobAttack,"A different state then expected")
+	
+	# Test that the second mob transitions into the MobAttack state
+	# Since we can't await two signals at once, we periodically try the state assert
+	var second_state: State = second_mob.get_current_state()
+	assert_not_null(second_state, "Second mob has no state")
+	var mob_has_transitioned = func():
+		return second_state is MobAttack
+	assert_true(await wait_until(mob_has_transitioned, 10, 1),"Mob should have transitioned")
