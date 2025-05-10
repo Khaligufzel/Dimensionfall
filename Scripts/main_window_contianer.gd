db3ffa4bfe0fb4f75dd37c3fbb74036509a58575
snipercup <50166150+snipercup@users.noreply.github.com>
2025-05-10 22:32:06 +0200

    Implement visibility toggle

--- Scripts/main_window_contianer.gd
+++ Scripts/main_window_contianer.gd
@@ -5,6 +5,7 @@ extends Control
 @export var header_panel_container: PanelContainer = null
 @export var body_panel_container: PanelContainer = null
 @export var header_label: Label = null
+@export var close_button: Button = null
 
 
 # Text to display in the header
@@ -14,10 +15,21 @@ extends Control
 @export var body_scene: PackedScene = null
 
 func _ready():
-	# Set the title text if a Label exists in the header
 	header_label.text = header_text
 
-	# Instantiate and add the body scene to the body container
 	if body_scene:
 		var instance = body_scene.instantiate()
 		body_panel_container.add_child(instance)
+
+	# Connect close button to hide the window
+	if close_button:
+		close_button.pressed.connect(_on_close_button_pressed)
+
+
+func _on_close_button_pressed():
+	visible = false
+
+func _input(event):
+	# Check if we need to hide based on what input action the child window uses
+	if body_scene.input_action != "" and event.is_action_pressed(body_scene.input_action):
+		visible = not visible
