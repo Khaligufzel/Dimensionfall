00f5c576e35c950b480df5800099d18684d7b9fd
Elshad Abbasov <elshad200131@gmail.com>
2025-05-26 10:50:37 +0400

    UI_Click now plays in scene_loader.tscn
    Using global scene Sfx.tscn, we moved a bit of code from UISFX.gd to Sfx.gd and made it so that the sounds are played from Sfx.tscn instead of UISFX.tscn

--- Sounds/SFX/UI/uisfx.gd
+++ Sounds/SFX/UI/uisfx.gd
@@ -2,23 +2,8 @@ extends Node
 
 @export var root_path : NodePath
 
-# create audio player instances
-@onready var sounds = {
-	&"UI_Hover" : AudioStreamPlayer.new(),
-	&"UI_Click" : AudioStreamPlayer.new(),
-	}
-
 func _ready() -> void:
 	assert(root_path != null, "Empty root path for Interface Sounds!")
-
-	# set up audio stream players and load sound files
-	for i in sounds.keys():
-		sounds[i].stream = load("res://Sounds/SFX/UI/" + str(i) + ".wav")
-		# assign output mixer bus
-		sounds[i].bus = &"Sfx"
-		# add them to the scene tree
-		add_child(sounds[i])
-
 	# connect signals to the method that plays the sounds
 	install_sounds(get_node(root_path))
 
@@ -26,23 +11,18 @@ func _ready() -> void:
 func install_sounds(node: Node) -> void:
 	for i in node.get_children():
 		if i is Button:
-			i.mouse_entered.connect( ui_sfx_play.bind(&"UI_Hover") )
-			i.pressed.connect( ui_sfx_play.bind(&"UI_Click") )
+			i.mouse_entered.connect( Sfx.ui_sfx_play.bind(&"UI_Hover") )
+			i.pressed.connect( Sfx.ui_sfx_play.bind(&"UI_Click") )
 		elif i is OptionButton:
-			i.mouse_entered.connect( ui_sfx_play.bind(&"UI_Hover") )
-			i.pressed.connect( ui_sfx_play.bind(&"UI_Click") )
+			i.mouse_entered.connect( Sfx.ui_sfx_play.bind(&"UI_Hover") )
+			i.pressed.connect( Sfx.ui_sfx_play.bind(&"UI_Click") )
 		elif i is TextureButton:
-			i.mouse_entered.connect( ui_sfx_play.bind(&"UI_Hover") )
-			i.pressed.connect( ui_sfx_play.bind(&"UI_Click") )
+			i.mouse_entered.connect( Sfx.ui_sfx_play.bind(&"UI_Hover") )
+			i.pressed.connect( Sfx.ui_sfx_play.bind(&"UI_Click") )
 		#elif i is TabContainer:
-		#	i.tab_hovered.connect( ui_sfx_play.bind(&"UI_Hover") )
-		#	i.tab_clicked.connect( ui_sfx_play.bind(&"UI_Hover") )
-		elif i is MarginContainer:
-			i.mouse_entered.connect( ui_sfx_play.bind(&"UI_Hover") )
-		#	i.pressed.connect( ui_sfx_play.bind(&"UI_Click") )
+		#	i.tab_hovered.connect( Sfx.ui_sfx_play.bind(&"UI_Hover") )
+		#	i.tab_clicked.connect( Sfx.ui_sfx_play.bind(&"UI_Hover") )
+		#elif i is MarginContainer:
+		#	i.mouse_entered.connect( Sfx.ui_sfx_play.bind(&"UI_Hover") )
+		#	i.pressed.connect( Sfx.ui_sfx_play.bind(&"UI_Click") )
 		install_sounds(i)
-
-
-func ui_sfx_play(sound : String) -> void:
-	print("Playing sound:", sound)
-	sounds[sound].play()
