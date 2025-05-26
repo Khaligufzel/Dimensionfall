aee588230bbce9699a1e0072877b03f7093a0dab
snipercup <50166150+snipercup@users.noreply.github.com>
2025-05-26 10:50:30 +0200

    Let playlist loop and repeat, stop when game ends

--- Sounds/Music/Music.gd
+++ Sounds/Music/Music.gd
@@ -26,6 +26,14 @@ var TRACKS = {
 var current_theme: int = THEMES.PEACE
 var is_repeating: bool = true
 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Connect to the Helper.signal_broker.game_started signal
+	Helper.signal_broker.game_started.connect(_on_game_started)
+	Helper.signal_broker.game_loaded.connect(_on_game_loaded)
+	Helper.signal_broker.game_ended.connect(_on_game_ended)
+
 func play_theme(theme: int, repeat_themes: bool = true):
 	if current_theme != theme or !StreamPlayer.playing:
 		is_repeating = false # Prevent accidentally starting an old track playing
@@ -45,10 +53,17 @@ func replay_current_theme():
 	StreamPlayer.stream = theme_tracks[randi() % theme_tracks.size()]
 	StreamPlayer.play()
 
-func _on_gameplay_music_peace_finished():
-	GameplayMusicPlayer.stop()
-	await get_tree().create_timer(10.0).timeout
-	GameplayMusicPlayer.play()
-
 func gameplay_music_stop():
 	GameplayMusicPlayer.stop()
+
+# Function for handling game started signal
+func _on_game_started():
+	GameplayMusicPlayer.play()
+	
+# Function for handling game ended signal
+func _on_game_ended():
+	GameplayMusicPlayer.stop()
+	
+# Function for handling game loaded signal
+func _on_game_loaded():
+	GameplayMusicPlayer.play()
