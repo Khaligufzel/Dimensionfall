f622f0c0659d343831b79f9038f25c3b43b685da
snipercup <50166150+snipercup@users.noreply.github.com>
2025-06-04 22:05:40 +0200

    Introduce wrapper

--- Sounds/SFX/sfx.gd
+++ Sounds/SFX/sfx.gd
@@ -1,14 +1,36 @@
 extends Node
 
 # Audio players
-@onready var stream_player: AudioStreamPlayer = $AudioStreamPlayer
-@onready var movement_player: AudioStreamPlayer = $MovementSFXPlayer
+@onready var stream_wrapper := SFXPlayerWrapper.new($AudioStreamPlayer)
+@onready var movement_wrapper := SFXPlayerWrapper.new($MovementSFXPlayer)
 
 # UI sound effects mapped by name
 @onready var ui_sounds := {
 	&"UI_Hover": $UI_Hover,
 	&"UI_Click": $UI_Click,
 }
+
+# --- Inner class that wraps an AudioStreamPlayer and its logic ---
+class SFXPlayerWrapper:
+	var player: AudioStreamPlayer
+	var current_sfx: int = -1
+
+	func _init(_player: AudioStreamPlayer) -> void:
+		player = _player
+
+	func stop() -> void:
+		if player:
+			player.stop()
+
+	func play_random(tracks: Array) -> void:
+		if tracks.is_empty():
+			return
+		player.stream = tracks.pick_random()
+		player.play()
+
+	func is_playing() -> bool:
+		return player and player.playing
+
 
 # Enum to reference sound effects
 enum SFX {
@@ -29,35 +51,25 @@ var tracks := {
 
 # Track current SFX and repeat mode
 var current_sfx: int = SFX.WALKING_GRASS
-var is_repeating: bool = false
 
 func play_sfx(sfx: int, repeat_sfx: bool = true):
-	if current_sfx != sfx or !stream_player.playing or !movement_player.playing:
-		is_repeating = false # Prevent accidentally starting an old track playing
-								# again when next command is stop()
-		stream_player.stop()
-		movement_player.stop()
-		
-		is_repeating = repeat_sfx
+	var wrapper: SFXPlayerWrapper = movement_wrapper if sfx == SFX.WALKING_GRASS else stream_wrapper
+
+	if current_sfx != sfx or not wrapper.is_playing():
+		stream_wrapper.stop()
+		movement_wrapper.stop()
+
 		current_sfx = sfx
-		
-		var sfx_tracks: Array = tracks[current_sfx]
-		if sfx_tracks != []:
-			if current_sfx == SFX.WALKING_GRASS:
-				movement_player.stream = sfx_tracks[randi() % sfx_tracks.size()]
-				movement_player.play()
-			else:
-				stream_player.stream = sfx_tracks[randi() % sfx_tracks.size()]
-				stream_player.play()
+
+		var sfx_tracks: Array = tracks.get(current_sfx, [])
+		wrapper.play_random(sfx_tracks)
+
 
 func replay_current_sfx():
-	var sfx_tracks: Array = tracks[current_sfx]
-	if current_sfx == SFX.WALKING_GRASS:
-		movement_player.stream = sfx_tracks[randi() % sfx_tracks.size()]
-		movement_player.play()
-	else:
-		stream_player.stream = sfx_tracks[randi() % sfx_tracks.size()]
-		stream_player.play()
+	var sfx_tracks: Array = tracks.get(current_sfx, [])
+	var wrapper: SFXPlayerWrapper = movement_wrapper if current_sfx == SFX.WALKING_GRASS else stream_wrapper
+	wrapper.play_random(sfx_tracks)
+
 
 func ui_sfx_play(sound : String):
 	ui_sounds[sound].play()
@@ -66,7 +78,7 @@ func _on_audio_stream_player_finished():
 	gameplay_sfx_stop()
 
 func gameplay_sfx_stop():
-	stream_player.stop()
+	stream_wrapper.stop()
 
 func movement_sfx_stop():
-	movement_player.stop()
+	movement_wrapper.stop()
